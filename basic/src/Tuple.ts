f6b39b3aab6db71bda2dd30a2f0f88bbc8acb6a0
SystemSolution21 <systemsolution21@gmail.com>
2026-05-17 16:07:16 +0900

    modified

--- basic/src/Tuple.ts
+++ basic/src/Tuple.ts
@@ -1,5 +1,5 @@
 // Tuple
-let person: [string, number] = ['John', 30];
+let person: [string, number] = ['John', 35];
 console.log(person);
 let [pName, pAge] = person; // destructuring
-console.log(`Hi, my name is ${pName} and i'm, ${pAge} years old.`);
+console.log(`Hi, my name is ${pName} and i'm ${pAge} years old.`);
