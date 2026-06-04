c0c79cbba0ffe50424aaec16ff4d39ac3c11f076
SystemSolution21 <systemsolution21@gmail.com>
2026-06-04 16:38:04 +0900

    modified: basic/src/ErrorHandling.ts

--- basic/src/ErrorHandling.ts
+++ basic/src/ErrorHandling.ts
@@ -17,3 +17,90 @@ try {
     }
 }
 
+// Error Custom Class
+// Define Validation Error
+class ValidationError extends Error {
+    field?: string | undefined;
+
+    constructor(message: string, field?: string) {
+        super(message);
+        this.name = 'ValidationError';
+        this.field = field;
+    }
+}
+
+// Database Error Code
+enum DbErrCode {
+    Protocol = 5001
+}
+
+// Define Database Error
+class DatabaseError extends Error {
+    code: number;
+
+    constructor(message: string, code: number) {
+        super(message);
+        this.name = 'DatabaseError';
+        this.code = code;
+    }
+}
+
+// Define User
+interface User {
+    name: string;
+    email: string;
+};
+
+// User Validation
+function validateUser(user: User): void {
+    if (!user.name) {
+        throw new ValidationError('Name is required', 'name');
+    }
+    if (!user.email) {
+        throw new ValidationError('Email is required', 'email');
+    }
+    if (!user.email.includes('@')) {
+        throw new ValidationError('Invalid email format', 'email');
+    }
+}
+
+// Database Connection Validation
+async function ValidateDatabaseConnection(dburi: string): Promise<boolean> {
+    if (!dburi.startsWith('mongodb://') && !dburi.startsWith('mysql://')) {
+        throw new DatabaseError('Invalid database connection URI protocol', DbErrCode.Protocol);
+    }
+    return true
+}
+
+// User Registration
+async function registerUser(user: User, dburi: string) {
+    try {
+        // Validate User
+        validateUser(user);
+        // Validate Database Connection
+        await ValidateDatabaseConnection(dburi);
+        // log
+        console.log(`Database connection is valid`);
+        console.log(`User is registered: ${user.name}, ${user.email}`);
+        // Register User
+    } catch (error) {
+        if (error instanceof ValidationError) {
+            console.error(`Validation Error [Field: ${error.field}]: ${error.message}`);
+        } else if (error instanceof DatabaseError) {
+            console.error(`Database Error [Code: ${error.code}]: ${error.message}`);
+        } else {
+            console.error('An unexpected error occurred:', error);
+        }
+    }
+}
+
+// Usage
+async function runUserRegistration() {
+    await registerUser({ name: "John Doe", email: "john.doe@example.com" }, 'mongodb://localhost:27017');
+    await registerUser({ name: "", email: "john.doe@example.com" }, 'mongodb://localhost:27017');
+    await registerUser({ name: "John Doe", email: "john.doeexample.com" }, 'mongodb://localhost:27017');
+    await registerUser({ name: "John Doe", email: "john.doe@example.com" }, 'mysql://localhost:3306');
+    await registerUser({ name: "John Doe", email: "john.doe@example.com" }, 'postgresql://localhost:5432');
+
+}
+runUserRegistration();
